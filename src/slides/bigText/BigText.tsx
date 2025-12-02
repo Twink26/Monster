549be302c5c49fbee90b1445594f5e3afd4a724c
Twinkle Rana <startwinklerana@gmail.com>
2025-12-03 01:13:46 +0530

    bigtext

---
+++ src/slides/bigText/BigText.tsx
@@ -0,0 +1,15 @@
+export default function BigText(){
+  
+  return (
+    <section
+      className="min-h-screen w-screen overflow-hidden bg-black text-[#90ED27] text-center"
+    >
+      <h2 className="w-full gap-[1vw] px-10 text-center font-black flex flex-col uppercase leading-[.7]">
+        <span className="text-[20vw] pt-12 text-orange-500">Full</span>
+        <span className="text-[12vw] md:[20vw] ">Throttle </span>
+        <span className="text-[12vw] md:[20vw]">Maximum </span>
+        <span className="text-[20vw] md:[12vw] pt-1 text-orange-500">Energy </span>
+      </h2>
+    </section>
+  );
+};
